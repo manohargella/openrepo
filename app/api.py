cc6300b6ac640b228442b6d9fab04aa9bfdf3b28
Manohar Gella <manohargella272@gmail.com>
2024-01-11 01:05:43 +0530

    flask int

---
+++ app/api.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Sample data
+sample_data = {
+    'message': 'Hello, this is a sample API response!',
+    'status': 'success'
+}
+
+# Route to handle API requests
+@app.route('/api/sample', methods=['GET'])
+def get_sample_data():
+    return jsonify(sample_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
