9c0262684c211d21023cf507eab438fce5d591f7
Manohar Gella <manohargella272@gmail.com>
2024-04-30 18:05:27 +0530

    Sum server

--- server.js
+++ server.js
@@ -1,38 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { json } = require('express');
-const app = express();
-const port = 4050;
+const epxress = require('epxress');
+const app = epxress();
+const cors = require('cors')
 
-app.use(json());
-app.use(cors());
-
-app.get('/calculate', (req, res) => {
-    const x = parseFloat(req.query.x);
-    const y = parseFloat(req.query.y);
-    const op = req.query.op;
-    let result;
-
-    if (op === '`') {
-        result = x + y;
-    } else if (op === '-') {
-        result = x - y;
-    } else if (op === '*') {
-        result = x * y;
-    } else if(op === '%'){
-        result = x%y;
-    }else if (op === '/') {
-        if (y === 0) {
-            return res.status(400).send("Division by zero error");
-        }
-        result = x / y;
-    } else {
-        return res.status(400).send("Invalid operation");
-    }
-
-    res.send(result.toString());
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+app.get('/sum', (req, res) => {
+    res.send("Sum server");
+} )
