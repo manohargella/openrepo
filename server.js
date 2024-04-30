fde45f3b935f5e5c0e2f852f10b7e8e0aacffe77
Manohar Gella <manohargella272@gmail.com>
2024-04-30 18:07:18 +0530

    update sum server

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const epxress = require('epxress');
-const app = epxress();
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000; // Use the port assigned by Hostinger or default to 3000
+
+app.use(cors()); // Enable CORS middleware
 
 app.get('/sum', (req, res) => {
     res.send("Sum server");
-} )
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
